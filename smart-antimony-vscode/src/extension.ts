9c254b41a933c098a00e52a12530f24f1b92d57b
mastevb <bochenma@cs.washington.edu>
2021-10-16 20:42:05 -0700

    changed package.json

--- smart-antimony-vscode/src/extension.ts
+++ smart-antimony-vscode/src/extension.ts
@@ -1,5 +1,3 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
 // this method is called when your extension is activated
@@ -16,7 +14,7 @@ export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('smart-antimony-vscode.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from smart-antimony-vscode!');
+		vscode.window.showInformationMessage('Hello from smart-antimony-vscode!');
 	});
 
 	context.subscriptions.push(disposable);
